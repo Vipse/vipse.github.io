52fb741f2b9d86bba35b6bf50ea4854f696b4917
Vladimir Zubritsky <v.zubritsky@abiatec.com>
2019-05-09 18:44:38 +0300

    added animation

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,31 @@ $(document).ready(function(){
     animateOut: 'slideOutLeft',
     animateIn: 'fadeInRight',
     smartSpeed: 1500,
-  })
+  });
+  $('.advantages-item').on('click', (e)=>{
+
+    $('.advantages-item').removeClass('active');
+
+
+    if(e.target.classList.contains('advantages-item')) {
+      e.target.classList.add('active');
+    } else {
+      e.target.parentNode.classList.add('active');
+    }
+
+
+    $('.advantage-description-title').css({opacity: 0, top: '50%'}).animate({
+      top: "0%",
+      opacity: 1,
+    }, {duration: 700, queue: false});
+    setTimeout(()=>{
+      $('.advantage-description-content').css({opacity: 0, top: '50%'}).animate({
+        top: "0%",
+        opacity: 1,
+      }, {duration: 700, queue: false});
+    }, 150);
+
+
+  });
+  console.log(123)
 });
