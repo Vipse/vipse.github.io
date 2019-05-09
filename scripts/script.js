83992f27834b0494da368f8351ce56d4b21e7c28
Vladimir Zubritsky <v.zubritsky@abiatec.com>
2019-05-09 16:25:46 +0300

    desktop

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    // autoplay: true,
+    items: 1,
+    animateOut: 'slideOutLeft',
+    animateIn: 'fadeInRight',
+    smartSpeed: 1500,
+  })
+});
