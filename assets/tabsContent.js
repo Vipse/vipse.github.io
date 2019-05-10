67de6d3a5931062891c971c3330d73a8ab63b993
Vladimir Zubritsky <v.zubritsky@abiatec.com>
2019-05-10 05:42:01 +0300

    added js

---
+++ assets/tabsContent.js
@@ -0,0 +1,255 @@
+const tabsContent = [
+  {
+    id: 0,
+    name: 'Deutschland',
+    items: [
+      {
+        photo: 'photo_deutschland_1.png',
+        itemTitle: 'Deutschland',
+        secondaryDescription: 'Steuern? Nein, danke.',
+        mainDescription: 'Die meisten Steuerberater sind Buchhalter-Persönlichkeiten, die im besten Fall Ihre ' +
+          'Bilanzen in Ordnung halten, sind jedoch in den wenigsten Fällen selbst erfolgreiche Unternehmer mit ' +
+          'unternehmerischer Praxis. Es gibt selbst im klassischen deutschen Steuerrecht viel mehr Möglichkeiten ' +
+          'als Sie vielleicht wissen. Mithilfe der Strategien der besten Unternehmer und Investoren können Sie ' +
+          'als Kleinunternehmer entspannt wachsen und als Großunternehmer steueroptimiert Vermögen aufbauen.',
+      },
+      {
+        photo: 'photo_deutschland_2.png',
+        itemTitle: 'Stiftungsgestaltung',
+        secondaryDescription: '0-15 % Steuern in Deutschland.',
+        mainDescription: 'Mit einer gemeinnützigen oder (unternehmensverbundenen) privaten Stiftung können Sie ' +
+          'sogar in Deutschland weiterhin leben, jedoch sicher vor Enteignung oder Erbschaftsteuer Ihr Vermögen ' +
+          'schützen und extrem steueroptimiert vermehren. Und bei einem Verkauf Ihres Unternehmens zahlen Sie nicht ' +
+          'bis zu 42 % sondern nur 0,75 % an Steuern.',
+      },
+      {
+        photo: 'photo_deutschland_3.png',
+        itemTitle: 'Genossenschaften',
+        secondaryDescription: 'Steuerfreie Mieteinnahmen.',
+        mainDescription: 'Mithilfe einer Genossenschaft, dem Zusammenschluss gleichdenkender Menschen ' +
+          '(Familienmitglieder), können Sie Mieteinnahmen steuerfrei gestalten, schützen sich vor Gläubigern, ' +
+          'Scheidungsrisiken, Pflegebedürftigkeit und privater Not. Ihr direktes Vermögen ist Genossenschaftsanteil ' +
+          'und Ihre Immobilien sind Sondervermögen der Genossenschaft. Sogar Erbschaftsthemen können Sie hiermit ' +
+          'zu Lebzeiten adäquat lösen.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Wirtschaftsvereine',
+        secondaryDescription: 'Machen Sie’s wie der ADAC.',
+        mainDescription: 'Es gibt in Verbindung mit Malta, UK und anderen Ländern extrem spannende Möglichkeiten ' +
+          'in der Gestaltung von Wirtschaftsvereinen, auch und gerade in Deutschland. Die EWIV als Spezialfall ist ' +
+          'eine international abgestimmte Rechtsform zur Förderung der europäischen Zusammenarbeit. Sie zahlt weder ' +
+          'Gewerbe- noch Körperschaftssteuer. Dafür darf sie keine Gewinne machen; diese müssen an die Mitglieder ' +
+          'abgeführt werden, die die Gewinne dann wieder bei sich versteuern. Allerdings kann sie Rücklagen bilden.',
+      },
+    ]
+  },
+  {
+    id: 1,
+    name: 'Österreich',
+    items: [
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Zypern-Organschaft',
+        secondaryDescription: 'Steuerfrei in Österreich.',
+        mainDescription: 'Wem Dubai zu fremdartig, Malta zu steinig und Zypern zu sonnig ist, der kann im schönen ' +
+          'Nachbarland Deutschlands und der Schweiz ein einfaches, jedoch wirkungsvolles Organschaftsmodell für ' +
+          'sich nutzen, um Ihre Einkommenssteuer dauerhaft auf 0% abzusenken. Grundlage hierfür sind das ' +
+          'Doppelbesteuerungsabkommen (DBA) zwischen Zypern und Österreich und ein hier festgelegter ' +
+          'Steuervorbehalt. Mehr dazu in unserem spannenden Kurs.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Stiftungsgestaltung',
+        secondaryDescription: '0-15 % Steuern in Deutschland.',
+        mainDescription: 'Eine deutsche gemeinnützige oder (unternehmensverbundene) private Stiftung können Sie ' +
+          'auch mit einem österreichischen Wohnsitz nutzen. Auch Liechtenstein bietet sich als diskreter Partner ' +
+          'für erfolgreiche Unternehmer an, da eine PVS (private Vermögensstruktur) fast steuerfrei Vermögen ' +
+          'vermehren kann. So können Sie Unternehmens- und Immobilienvermögen in Österreich, Europa und weltweit ' +
+          'schützen, gestalten und effektiv kaum mehr Steuern zahlen.',
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Schweiz',
+    items: [
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Schweiz',
+        secondaryDescription: 'Sicherheit und Wohlstand.',
+        mainDescription: 'Die Schweiz ist das Land meiner Träume und auch für viele Auswanderer und smarte ' +
+          'Unternehmer ein ideales Ziel. Der ideale Wohnsitz für international steueroptimierte ' +
+          'Unternehmensgestaltung. Wohlstand, Freiheit und Sicherheit, wie man sie sich nur wünschen kann. Die ' +
+          'Firmengründung und das Einwandern sind extrem schwierig geworden. Wir zeigen Ihnen, wie es dennoch gelingt.',
+      },
+    ]
+  },
+  {
+    id: 3,
+    name: 'Europa',
+    items: [
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Bulgarien',
+        secondaryDescription: '10 % Flat-Tax genießen.',
+        mainDescription: 'Führende deutsche Software-Unternehmer haben dieses vollkommen unterschätzte Land bereits ' +
+          'entdeckt. Als wahres Steuerparadies mit dynamischen Mitarbeitern, hohen Förderungen durch den Staat und ' +
+          'sehr geringen Mieten, Lebenshaltungskosten und Abgaben kann Bulgarien auch für Sie und Ihr Unternehmen ' +
+          'zur neuen Heimat werden. Hier können Sie extrem schnell Wohlstand aufbauen.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Estland',
+        secondaryDescription: '0 % Steuern in der EU.',
+        mainDescription: 'Unser Geheimtipp No. 1: Den Meisten unbekannt, bietet Estland in Europa smarten ' +
+          'Internet-Unternehmern die beste Möglichkeit, mit einem Euro-Konto und einer EU-Steuernummer legal ' +
+          'dauerhaft 0 % Körperschaftsteuern zu zahlen. Zudem bietet das Land sehr gut ausgebildete Mitarbeiter ' +
+          'und ein extrem innovativ-modernes Geschäftsklima.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Irland',
+        secondaryDescription: '12,5 % im Software-Paradies.',
+        mainDescription: 'Apple, Google und Facebook sind schon lange dort. Viele Milliarden der größten ' +
+          'Software-Konzerne der Welt fließen über die Steuerinsel im Feenland. Gut ausgebildete Mitarbeiter ' +
+          'und ein liberales Klima machen die Insel zum führenden Standort für IT in Europa. Kaum irgendwo sonst ' +
+          'können Sie so einfach und steueroptimiert wirtschaften wie in Irland. ',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Liechtenstein',
+        secondaryDescription: 'Das diskrete Fürstentum.',
+        mainDescription: 'Ein attraktiver Gewinnsteuersatz von 12.5%, keine Quellenbesteuerung auf Dividenden, ' +
+          'Lizenzen und Zinsen, ein striktes Bankgeheimnis, zahlreiche Doppelbesteuerungsabkommen sowie eine ' +
+          'bürgernahe Steuerverwaltung machen Liechtenstein extrem interessant. Und mit einer Stiftung im ' +
+          'Fürstentum (PVS) lassen sich beinahe steuerfrei internationale Firmenbeteiligungen und Aktienvermögen ' +
+          'aufbauen.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Malta',
+        secondaryDescription: '5 % Steuern genießen.',
+        mainDescription: 'Große Unternehmen wie die Deutsche Bank, Playmobil, BMW, Sixt, Heidelberger Cement, ' +
+          'Lufthansa u.v.a agieren global. Das können Sie im Rahmen der Freizügigkeit in der EU ebenso und ' +
+          'profitieren von effektiv 5 % Steuerbelastung auf der schönen Kreuzfahrerinsel mit der höchsten Dichte ' +
+          'an Kirchen weltweit. Auch mit einem deutschen Wohnsitz sind sehr spannende Konzepte umsetzbar.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Tschechien',
+        secondaryDescription: 'Der unterschätzte Nachbar.',
+        mainDescription: 'Das Land in unserer unmittelbaren Nachbarschaft kennt kein Außensteuergesetz und lässt ' +
+          'internationale Unternehmer unkompliziert gewähren. Zugleich können bei Einkommen von bis zu 80.000 € mit ' +
+          'einer Einzelfirma nur 15 % auf den Umsatz bezahlt werden: inkl. Einkommensteuer, europäischer ' +
+          'Krankenversicherung und Sozialabgaben. Und nicht zuletzt ist Prag eine der schönsten Hauptstädte der Welt.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Zypern',
+        secondaryDescription: 'Steuerparadies im Mittelmeer.',
+        mainDescription: 'Simply-Non-Dom. Über 300 Sonnentage im Jahr. Eine Körperschaftssteuer von 12,5%. Keine ' +
+          'Gewerbesteuer. Keine Besteuerung der Dividenden einer Zypern Gesellschaft. Es gibt viele Gründe, Zypern ' +
+          'im Rahmen einer intelligenten Unternehmensstruktur einzusetzen. Gerade viele ortsunabhängige Unternehmer ' +
+          'und Ecommerce-Händler schätzen die Insel für Ihre enormen Vorteile.',
+      },
+
+    ]
+  },
+  {
+    id: 4,
+    name: 'Asien',
+    items: [
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Australien',
+        secondaryDescription: 'Sicherheit + Lebensqualität.',
+        mainDescription: 'Schneeweiße Sandstrände, farbenprächtige Unterwasserwelten und das pulsierende Sydney. ' +
+          'Australien ist eine der sichersten und wohlhabendsten Industrienationen mit einem der besten ' +
+          'Bildungssysteme der Welt. Trotz 30 % Körperschaftssteuer zieht es immer mehr Deutsche zu Recht auf den 5. ' +
+          'Kontinent. So gelingt das Auswandern nach Australien. ',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Hong Kong',
+        secondaryDescription: 'Steuerfrei + Bankgeheimnis.',
+        mainDescription: 'Bis 2047 der wahrscheinlich sicherste Ort der Welt für Ihr Vermögen. Hongkong kennt keine ' +
+          'Besteuerung für nicht vor Ort erzielte Gewinne, ist der beste Einstieg in den asiatischen Markt, hat ein ' +
+          'extrem sicheres Bankgeheimnis in der Verfassung und ist ein pulsierender Hotspot zum Leben und Arbeiten. ' +
+          'Mein weltweiter Favorit.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Malaysia',
+        secondaryDescription: '3 % Steuern zahlen.',
+        mainDescription: 'Extrem günstig ein Leben im Paradies führen. Richtig aufgestellt, können Sie in Malaysia ' +
+          'privat komplett steuerfrei leben und auch pauschal nur 3 % auf die Gewinne mit einem Labuan-Unternehmen ' +
+          'bezahlen. Neben Hong Kong und Singapur verfügt Labuan über die beste Business-Infrastruktur in ' +
+          'Südostasien. Lernen Sie Malaysia kennen, ein kaum bekanntes Juwel. ',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Emirate',
+        secondaryDescription: 'Oase in der Wüste.',
+        mainDescription: 'Wer schon immer in den Emiraten residieren, legal gar keine Steuern mehr gewerblich wie ' +
+          'privat zahlen, jedoch ohne Araber alles selbst beherrschen wollte, findet in den Emiraten eine wahre Oase ' +
+          'des Reichtums, der Freiheit und der Sicherheit. Das Bankensystem ist sicher, alle Geschäfte komplett ' +
+          'anonym und Sie können recht einfach in die VAE einwandern und steuerfrei leben.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Singapur.',
+        secondaryDescription: 'Steuerfrei und diskret.',
+        mainDescription: 'Die sauberste Stadt der Welt. Ein gewaltfreier, extrem sicherer Ort. Und keine Steuern ' +
+          'auf ausländische Gewinne. Singapur ist eine Traumstadt und Menschen, die Wohlstand, Ordnung und ' +
+          'wirtschaftliche Freiheit zusammen mit perfektem Wetter genießen wollen, erfahren hier alle Details zur ' +
+          'Firmengründung und den VISA-Bestimmungen. ',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Thailand.',
+        secondaryDescription: 'Auswandern ins Paradies.',
+        mainDescription: 'Die ganze Welt liebt Thailand: Traumhafte Sandstrände, allerorten glückliche Menschen, ' +
+          'wundervolles und gesundes Essen, eine uralte und erhabene Kultur und eine Lebensfreude, die einfach ' +
+          'ansteckend ist. Wollen Sie mithilfe der Territorialbesteuerung sogar steuerfrei im Paradies leben und ' +
+          'vom Boom dieses hoch-dynamischen Landes profitieren? Dann freuen Sie sich auf diesen Kurs.',
+      },
+
+    ]
+  },
+  {
+    id: 5,
+    name: 'Amerika',
+    items: [
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Kanada',
+        secondaryDescription: 'Pure Freude tanken.',
+        mainDescription: 'Eines der schönsten Länder der Welt, das beste Gesundheits- und Bildungssystem in Amerika, ' +
+          'eine starke Wirtschaft und weithin unberührte Natur. Es gibt viele Gründe, weshalb Kanada bei Deutschen ' +
+          'zum Auswandern so beliebt ist. Das Land kennt hohe Steuern und bietet doch ein extrem spannendes Modell ' +
+          'der Steuerfreiheit. So gelingt die neue Existenz in Kanada.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'Panama',
+        secondaryDescription: 'Steuerfrei nach Lateinamerika.',
+        mainDescription: 'Durch die Panama Papers ziemlich in Verruf gekommen, ist das schöne Land in Mittelamerika ' +
+          'kein Paradies mehr für Steuerhinterzieher. Doch für ehrliche Steueroptimierer oder Auswanderer ist es ' +
+          'noch immer extrem spannend: Nur $300 Pauschalsteuer pro Jahr und ein perfektes Klima lassen noch immer ' +
+          'viele Herzen höher schlagen.',
+      },
+      {
+        photo: 'photo_deutschland_4.png',
+        itemTitle: 'USA',
+        secondaryDescription: 'Leben auf der Überholspur.',
+        mainDescription: 'Als reichstes Land der Welt noch immer eines der beliebtesten Länder für Auswanderer. ' +
+          'Wollen Sie wissen, wie Sie in Puerto Rico nur 4 % Steuern zahlen können? Wollen Sie mit Tax Liens ' +
+          'garantiert 18 % pro Jahr verdienen? Dann erfahren Sie hier alle Details zur Firmengründung, dem ' +
+          'Steuerrecht und den hochkomplexen Visa-Bestimmungen. ',
+      },
+
+    ]
+  },
+  {},
+];
--- scripts/script.js
+++ scripts/script.js
@@ -23,15 +23,61 @@ $(document).ready(function(){
       top: "0%",
       opacity: 1,
     }, {duration: 700, queue: false});
-    $('.advantage-description-content').css({opacity: 0, top: '50%'});
-    setTimeout(()=>{
-      $('.advantage-description-content').animate({
-        top: "0%",
-        opacity: 1,
-      }, {duration: 700, queue: false});
-    }, 150);
+
+    $('.advantage-description-content').css({opacity: 0, top: '50%'}).animate({
+      top: "0%",
+      opacity: 1,
+    }, {duration: 850, queue: false});
 
 
   });
-  console.log(123)
+
+  $('button.country').on('click', (e) => {
+    const countryId = e.target.getAttribute('data-country-id');
+    const selectedCountryData = tabsContent.filter(country => country.id === +countryId)[0];
+    const newCountryData = selectedCountryData.items.map(item => (
+      `<div class="item"><div class="item-photo"><img src="assets/images/tabs/${item.photo}" alt=""></div>
+      <div class="item-description">
+      <div class="item-title">
+      ${item.itemTitle}
+      </div>
+      <div class="secondary-description">
+    ${item.secondaryDescription}
+    </div>
+    <div class="main-description">
+      ${item.mainDescription}
+    </div>
+    <div class="buttons">
+      <button class="read-manual">
+      Handbuch lesen
+    </button>
+    <button class="video">
+      <img src="assets/images/icon_play.svg" alt="">
+      <span>Videos anschauen</span>
+    </button>
+    </div>
+    </div>
+    </div>`
+    ));
+    const list = $('.country-tabs .container .list');
+    list.html(newCountryData);
+    $('button.country').removeClass('active');
+    e.target.classList.add('active');
+  })
+
+  $('button.open-close').on('click', (e)=>{
+    e.target.classList.toggle('open');
+    e.target.parentElement.parentElement.nextElementSibling.classList.toggle('open');
+  })
+
+
+
+  $(document).on('scroll', (e)=>{
+    const scrollTop = document.documentElement.scrollTop;
+    if(scrollTop) {
+      $('.header-top').addClass('scrolled');
+    } else {
+      $('.header-top').removeClass('scrolled');
+    }
+  })
 });
